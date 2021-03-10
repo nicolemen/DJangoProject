93bd885461cb63fdc9261acc8f0c38ddfd45dd96
nicolemen <nicole.men.21@bishops.com>
2021-03-09 22:33:06 -0800

    part 1

--- DJangoProject/polls/models.py
+++ DJangoProject/polls/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    posts = models.ForeignKey('Posts',on_delete=models.CASCADE ,)
+
+
+class Posts(model.Model):
--- DJangoProject/polls/views.py
+++ DJangoProject/polls/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class MainView(generic.DetailView):
+    template_name = 'polls/main.html'
+
+class LoginView(generic.DetailView):
+    template_name = 'polls/login.html'
